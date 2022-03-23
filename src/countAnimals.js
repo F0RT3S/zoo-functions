34ad39b7bba2d7cc6477b5c9da07d5c648fea875
felipe <fortes.felipe95@gmail.com>
2022-03-23 09:46:06 -0300

    Exercicio 05 - OK

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,39 +1,21 @@
 const data = require('../data/zoo_data');
-/* ### 5. Implemente a função `countAnimals`
 
-  Esta função é responsável por contabilizar a quantidade de animais de cada espécie.
+const allSpecies = data.species.reduce((acc, item) => {
+  acc[item.name] = item.residents.length;
+  return acc;
+}, {});
 
-**Observações técnicas**
-
-  - Se nenhum argumento for passado, retorna um objeto cujo o nome de cada espécie é uma chave desse objeto, e o total de animais dessa espécie é o seu valor;
-  - Com o argumento `{ specie: 'penguins' }`, retorna um número, a quantidade de pinguins no zoológico;
-  - Com o argumento `{ specie: 'giraffes', sex: 'female' }`, retorna um número, a quantidade de girafas do sexo feminino.
-
-**O que será avaliado**
-
-  - Sem parâmetros, retorna as espécies e sua quantidade;
-  - Recebendo como parâmetro um objeto com a chave `specie`, retorna um número, a quantidade de animais daquela espécie;
-  - Recebendo como parâmetro um objeto com a chave `specie` e `sex`, retorna um número, a quantidade de animais daquela espécie, no sexo selecionado.
-
---- */
-
-function countAnimals({ specie: name, sex: sexo }) {
-  // seu código aqui
-  const especies = data.species;
-  return especies.find((item) => (item.name === name ? item.residents.length : 'false'));
+function countAnimals(animal) {
+  if (!animal) {
+    return allSpecies;
+  }
+  const { specie, sex } = animal;
+  if (sex === undefined) {
+    return allSpecies[specie];
+  }
+  return data.species.find((item) => item.name === specie)
+    .residents.filter((item2) => item2.sex === sex).length;
 }
 
-console.log(countAnimals({ specie: 'lions' }));
+console.log(countAnimals({ specie: 'giraffes', sex: 'female' }));
 module.exports = countAnimals;
-
-// function countAnimals({ specie: name, sex: sexo }) {
-//   // seu código aqui
-//   const especies = data.species;
-//   const quantidade = especies.find((item) => {
-//     if (item.name === name) {
-//       const quantEspecieResidente = (item.residents.length);
-//       console.log(quantEspecieResidente);
-//     }
-//   });
-//   return quantidade;
-// }
